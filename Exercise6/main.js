c94d8227abd047d1afe79731c1df04c5232600f8
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-19 09:30:08 +0300

    Exercise one in javascript arrays

---
+++ Exercise6/main.js
@@ -0,0 +1,5 @@
+let colors = ["blue", "green", "yellow"];
+
+console.log(colors[0]); // Natiijada: blue  (Curiyeha 1-aad)
+console.log(colors[1]); // Natiijada: green (Curiyeha 2-aad)
+console.log(colors[2]); // Natiijada: yellow (Curiyeha 3-aad)
