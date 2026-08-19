164d8f93a65260bc01a62e7528b6d33eeb40d156
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-19 10:15:18 +0300

    Exercise 8

---
+++ Exercise8/main.js
@@ -0,0 +1,10 @@
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2026
+};
+
+console.log(car.make); // Output: Toyota
+console.log(car.model); // Output: Corolla
+console.log(car.year); // Output: 2020
+
