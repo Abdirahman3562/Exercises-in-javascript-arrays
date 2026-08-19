590035b6007e13e3ef2e9ec14dda47b057882c7b
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-19 10:57:32 +0300

    Exercise 10

---
+++ Exercise10/main.js
@@ -0,0 +1,20 @@
+let library = [
+    {
+            title: "waxaan ahay adoonkaaga",
+            author: "mohamed",
+            year: 2026,
+
+    },
+
+
+      {
+            title: "mucjisada aroorti",
+            author: "makel",
+            year: 2020,
+
+    }
+
+]
+
+console.log(library[0].title);
+console.log(library[1].author);
